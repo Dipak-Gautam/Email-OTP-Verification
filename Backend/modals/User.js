6610f68ca1dcec8cbf334ad7217cdb6c3ec5d216
Dipak-Gautam <anjangautam095@gmail.com>
2025-05-25 14:04:14 +0545

    feat: user schema updated

--- Backend/modals/User.js
+++ Backend/modals/User.js
@@ -1,27 +1,90 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    templateId: {
+      type: String,
+    },
+    secretCode: {
+      type: String,
+    },
+    emailConfig: {
+      subject: {
+        type: String,
+        default: "OTP Verification",
+      },
+      title: {
+        type: String,
+        default: "Verify your Email",
+      },
+      firstParagraph: {
+        type: String,
+        default:
+          "Hello\nThank you for signing up! Please confirm your email address by entering the code below:",
+      },
+      afterParagraph: {
+        type: String,
+        default:
+          "If you did not create an account, no further action is required. If you have any questions, feel free to contact our support team.",
+      },
+      footer: {
+        type: String,
+        default: "© 2025 Dipak Gautam. All rights reserved.",
+      },
+      titleColor: {
+        type: String,
+        default: "ffffff",
+      },
+      titleBackgroundColor: {
+        type: String,
+        default: "33e371",
+      },
+      optColor: {
+        type: String,
+        default: "00a63e",
+      },
+      optBackgroundColor: {
+        type: String,
+        default: "e8f5e9",
+      },
+      otpBorderColor: {
+        type: String,
+        default: "00a63e",
+      },
+      bodyColor: {
+        type: String,
+        default: "1d1d1d",
+      },
+      bodyBackgroundColor: {
+        type: String,
+        default: "ffffff",
+      },
+      footerColor: {
+        type: String,
+        default: "6f7686",
+      },
+      footerBackgroundColor: {
+        type: String,
+        default: "f4f4f4",
+      },
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  templateId: {
-    type: String,
-  },
-  secretCode: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 userSchema.pre("save", async function (next) {
   const user = this;
